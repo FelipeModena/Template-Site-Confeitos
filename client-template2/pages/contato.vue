9658db038e96926b09cf1fa824bb1f84270747f6
FelipeMatoso <73952306+FelipeModena@users.noreply.github.com>
2021-07-01 17:04:36 -0300

    Update contato.vue

--- client-template2/pages/contato.vue
+++ client-template2/pages/contato.vue
@@ -2,24 +2,41 @@
   <div class="px-4 my-4 cor-fundo-primaria">
     <div class="row">
       <FormsContato
-      id="contato-forms-contato"
-        class="col "
+        id="contato-forms-contato"
+        class="col"
         :ativo="index.forms.ativo"
         :listaIputs="index.forms.inputs"
         :mensagem="index.forms.mensagem"
       />
-      <ListaInformativa id="contato-lista-informativa" class="p-5 col" :contato="dadosCadastrais" />
+      <div class="col">
+        <h1 v-if="mobile" class="ml-5 text-center">
+          <strong>Fale conosco e faça seu orçamento!</strong>
+        </h1>
+        <ListaInformativa
+          id="contato-lista-informativa"
+          class="p-5 col w-100 text-center"
+          :contato="dadosCadastrais"
+        />
+      </div>
     </div>
 
-    <div class="my-4 mx-3" v-html="contato.iframeMapaGoogle" id="contato-iframe" v-show="contato.iframeMapaGoogle">
-      
-    </div>
+    <div
+      class="my-4 mx-3"
+      v-html="contato.iframeMapaGoogle"
+      id="contato-iframe"
+      v-show="contato.iframeMapaGoogle"
+    ></div>
   </div>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
 export default {
+  data() {
+    return {
+      mobile: false,
+    };
+  },
   computed: {
     ...mapGetters({
       index: "conteudo/index",
@@ -27,17 +44,31 @@ export default {
       contato: "conteudo/contato",
     }),
   },
+  mounted() {
+    if (document.documentElement.clientWidth <= 850) {
+      this.mobile = true;
+    }
+
+    addEventListener("resize", () => {
+      if (document.documentElement.clientWidth <= 850) {
+        this.mobile = true;
+      } else {
+        this.mobile = false;
+      }
+    });
+  },
 };
 </script>
 
 <style>
-#contato-lista-informativa{
-height: 100%;
+#contato-lista-informativa {
+  height: 500px;
+  width: 45%;
 }
-#contato-forms-contato input{
-width: 100%;
+#contato-forms-contato input {
+  width: 100%;
 }
-#contato-iframe iframe{
-width: 100%;
+#contato-iframe iframe {
+  width: 100%;
 }
 </style>
