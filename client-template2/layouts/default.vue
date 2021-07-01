9a0b5413d6efe10224302f65a49d95adf1073b68
FelipeMatoso <73952306+FelipeModena@users.noreply.github.com>
2021-07-01 14:43:21 -0300

    Update default.vue

--- client-template2/layouts/default.vue
+++ client-template2/layouts/default.vue
@@ -14,14 +14,14 @@
         <b-navbar-toggle target="nav-collapse" />
 
         <b-collapse id="nav-collapse" is-nav>
-          <b-navbar-nav id="nav-bar-links-topo"  class="ml-auto ">
+          <b-navbar-nav id="nav-bar-links-topo" class="ml-auto">
             <NuxtLink
               v-for="(headerOp, index) in headerOpcoes"
               :key="index"
               :to="headerOp.rota"
             >
               <li class="nav-bar-opcao-li">
-            <i :id="`nav-opcao-italico-` + index"></i>
+                <i :id="`nav-opcao-italico-` + index"></i>
                 <h5 :id="`nav-opcao` + index" @click="alteraMarcacao(index)">
                   {{ headerOp.titulo }}
                 </h5>
@@ -33,8 +33,8 @@
     </header>
     <main><Nuxt /></main>
     <footer>
-      <div id="footer-default-grid" class="cor-fundo-secundaria p-5">
-        <div class="ml-3">
+      <div id="footer-default-grid" class="cor-fundo-secundaria px-5 py-2">
+        <div>
           <div class="text-left">
             <img width="15%" src="~/assets/icones/imagem.png" alt="" />
             <span class="logotipo-cor">Logotipo</span>
@@ -51,19 +51,14 @@
             </h5>
           </NuxtLink>
         </div>
-        <div class="mr-3">
+        <div class="mt-3">
           <ListaInformativa :contato="dadosCadastrais" />
-          <img
-            class="float-right"
-            width="15%"
-            src="~/assets/icones/logo_sharkdata.png"
-            alt=""
-          />
         </div>
       </div>
 
       <div class="container py-3">
         2021 (c) Nome da empresa todos os direito reservadis
+        <img class="ml-3" width="10%" src="~/assets/icones/logo_sharkdata.png" alt="" />
       </div>
     </footer>
   </div>
@@ -80,24 +75,20 @@ export default {
   },
   mounted() {
     const nav = this.headerOpcoes.find((elemnt) => elemnt.rota === this.$route.name);
-    console.log(nav);
     if (nav !== undefined) {
-      $("#nav-opcao-italico-" + nav.id).addClass('circulo-marcacao-pagination');
-    } else
-      $("#nav-opcao-italico-" + 0).addClass('circulo-marcacao-pagination');
+      $("#nav-opcao-italico-" + nav.id).addClass("circulo-marcacao-pagination");
+    } else $("#nav-opcao-italico-" + 0).addClass("circulo-marcacao-pagination");
   },
   methods: {
     alteraMarcacao(e) {
-       console.log( $("#nav-opcao-italico-" + e).children());
-       console.log( $("#nav-opcao-italico-" + e));
       for (let index = 0; index < this.headerOpcoes.length; index++) {
-        $("#nav-opcao-italico-" + index).removeClass('circulo-marcacao-pagination');
+        $("#nav-opcao-italico-" + index).removeClass("circulo-marcacao-pagination");
       }
       if (e === undefined) {
-        $("#nav-opcao-italico-" + 0).addClass('circulo-marcacao-pagination');
+        $("#nav-opcao-italico-" + 0).addClass("circulo-marcacao-pagination");
         return;
       }
-      $("#nav-opcao-italico-" + e).addClass('circulo-marcacao-pagination');
+      $("#nav-opcao-italico-" + e).addClass("circulo-marcacao-pagination");
     },
   },
 };
@@ -116,16 +107,16 @@ export default {
   grid-template-columns: auto auto;
   width: 100%;
 }
-.nav-bar-opcao-li{
-  display:flex;
+.nav-bar-opcao-li {
+  display: flex;
 }
-.circulo-marcacao-pagination{
-    -webkit-border-radius:8px;
-    -moz-border-radius:8px;
-    border-radius:8px;
-    background:var(--cor-padrao-header-fonte);
-    margin: 8px 3px 0 0 ;
-    width:8px;
-    height:8px;
+.circulo-marcacao-pagination {
+  -webkit-border-radius: 8px;
+  -moz-border-radius: 8px;
+  border-radius: 8px;
+  background: var(--cor-padrao-header-fonte);
+  margin: 8px 3px 0 0;
+  width: 8px;
+  height: 8px;
 }
 </style>
